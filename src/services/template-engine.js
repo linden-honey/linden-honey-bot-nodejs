4c7e1e636434d8f80e7efcee3488bb753efd6d93
Alexander Babai <aliaksandr.babai@gmail.com>
2020-01-09 14:57:03 +0300

    refactor(services.template-engine): improvements
    * use class properties
    * improve templates resolving
    * lint

--- src/services/template-engine.js
+++ src/services/template-engine.js
@@ -1,12 +1,15 @@
 const fs = require('fs')
-const promisify = require('util').promisify
+const { promisify } = require('util')
 const Handlebars = require('handlebars')
 
 const readFile = promisify(fs.readFile)
-const DEFAULT_CONFIG = { folder: 'src/templates', extension: 'hbs' }
+const defaults = { folder: 'src/templates', extension: 'hbs' }
 
-module.exports = class TemplateEngine {
-    constructor({ folder = DEFAULT_CONFIG.folder, extension = DEFAULT_CONFIG.extension } = DEFAULT_CONFIG) {
+class TemplateEngine {
+    constructor({
+        folder = defaults.folder,
+        extension = defaults.extension,
+    } = defaults) {
         this.handlebars = Handlebars.create()
         this.folder = folder
         this.extension = extension
@@ -14,21 +17,23 @@ module.exports = class TemplateEngine {
         this.init()
     }
 
-    init() {
-        //register instance-specific helpers here
+    init = () => {
+        // register instance-specific helpers here
     }
 
-    async render(templateName, data) {
+    render = async (templateName, data) => {
         const template = await this.resolveTemplate(templateName)
         return template(data)
     }
 
-    async resolveTemplate(name) {
-        if (!this.templates.hasOwnProperty(name)) {
+    resolveTemplate = async (name) => {
+        if (!(name in this.templates)) {
             const source = await readFile(`${this.folder}/${name}.${this.extension}`)
             const template = this.handlebars.compile(source.toString())
-            return this.templates[name] = template
+            this.templates[name] = template
         }
         return this.templates[name]
     }
 }
+
+module.exports = TemplateEngine
