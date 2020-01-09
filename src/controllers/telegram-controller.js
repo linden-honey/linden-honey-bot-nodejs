ca46139254ffb359f1e29d97867abd325d6be9a7
Alexander Babai <aliaksandr.babai@gmail.com>
2020-01-09 17:19:14 +0300

    refactor(controllers): rewrite to express fortmat

--- src/controllers/telegram-controller.js
+++ src/controllers/telegram-controller.js
@@ -1,7 +1,17 @@
-exports.handleUpdate = async (ctx, next) => {
-    ctx.bot.handleUpdate(ctx.request.body, ctx.res)
-    ctx.body = ctx.request.body
-    ctx.status = 201
-    ctx.message = 'Telegram update has been successfully handled'
-    next()
+class TelegramController {
+    constructor({ bot }) {
+        this.bot = bot
+    }
+
+    handleUpdate = async (req, res) => {
+        if (req.is('json')) {
+            const { body } = req
+            this.bot.handleUpdate(body, res)
+            res.status(201).send('Telegram update has been successfully handled')
+        } else {
+            res.status(415).send('Incorrect update format')
+        }
+    }
 }
+
+module.exports = TelegramController
