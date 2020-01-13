4112fa832d1d1514e7e39ebddff6e89b1c204003
Alexander Babai <aliaksandr.babai@gmail.com>
2020-01-13 15:22:40 +0300

    add logging

--- src/controllers/telegram-controller.js
+++ src/controllers/telegram-controller.js
@@ -7,7 +7,9 @@ class TelegramController {
         if (req.is('json')) {
             const { body } = req
             try {
-                await this.bot.handleUpdate(body, res)
+                const result = await this.bot.handleUpdate(body, res)
+                console.log('BODY', body)
+                console.log('RESULT', result)
                 res.status(201).send('Telegram update has been successfully handled')
             } catch (e) {
                 const message = 'Error during update handling'
